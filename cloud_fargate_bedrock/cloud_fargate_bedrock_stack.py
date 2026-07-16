9705efbf53f1d2c4690b853ac905d9026f46388c
SANTHOSH P <santhosh11042007@gmail.com>
2026-07-16 21:07:06 +0530

    Update cloud_fargate_bedrock_stack.py

--- cloud_fargate_bedrock/cloud_fargate_bedrock_stack.py
+++ cloud_fargate_bedrock/cloud_fargate_bedrock_stack.py
@@ -4,50 +4,76 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_ecr as ecr,
+    aws_logs as logs,
 )
-
 from constructs import Construct
 
 
 class CloudFargateBedrockStack(Stack):
 
-    def __init__(
-        self,
-        scope: Construct,
-        construct_id: str,
-        **kwargs
-    ) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # -----------------------------
         # VPC
-        # -----------------------------
         vpc = ec2.Vpc(
             self,
             "CloudVpc",
             max_azs=2
         )
 
-        # -----------------------------
         # ECS Cluster
-        # -----------------------------
         cluster = ecs.Cluster(
             self,
             "CloudCluster",
             vpc=vpc
         )
 
-        # -----------------------------
-        # Amazon ECR Repository
-        # -----------------------------
+        # ECR Repository
         repository = ecr.Repository(
             self,
-            "CloudFargateBedrockRepository",
+            "CloudRepository",
             repository_name="cloud-fargate-bedrock",
             removal_policy=RemovalPolicy.DESTROY,
             empty_on_delete=True,
         )
 
-        # Outputs for verification
-        self.cluster = cluster
-        self.repository = repository
+        # CloudWatch Logs
+        log_group = logs.LogGroup(
+            self,
+            "CloudLogs",
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # Fargate Task Definition
+        task_definition = ecs.FargateTaskDefinition(
+            self,
+            "CloudTask",
+            cpu=256,
+            memory_limit_mib=512,
+        )
+
+        # Container
+        container = task_definition.add_container(
+            "CloudContainer",
+            image=ecs.ContainerImage.from_registry("nginx:latest"),
+            logging=ecs.LogDrivers.aws_logs(
+                stream_prefix="cloud",
+                log_group=log_group,
+            ),
+        )
+
+        container.add_port_mappings(
+            ecs.PortMapping(
+                container_port=80
+            )
+        )
+
+        # Fargate Service
+        ecs.FargateService(
+            self,
+            "CloudService",
+            cluster=cluster,
+            task_definition=task_definition,
+            desired_count=1,
+            assign_public_ip=True,
+        )
